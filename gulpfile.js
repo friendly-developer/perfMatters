66c16f2de3a25ec6198efe0d56bd491d435aa681
gvsvarun <gvsvarun@gmail.com>
2017-02-02 23:47:58 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var filesize = require('gulp-filesize');
+// var less = require('gulp-less');
+// var changed = require('gulp-changed');
+// var watch = require('gulp-watch');
+var htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('clean', function () {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('jsUglify',['clean'], function() {
+  return gulp.src('js/*.js')
+    //.pipe(concat('vendor.js'))
+    //.pipe(gulp.dest('dist'))
+    .pipe(filesize())
+    .pipe(uglify())
+    //.pipe(rename('perfMatters.min.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(filesize())
+    .on('error', gutil.log)
+});
+
+
+gulp.task('imagesMin', ['clean'], function() {
+  return gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+});
+gulp.task('minify',['clean'], function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('default', ['clean', 'jsUglify','minify','imagesMin'], function() {
+
+});
