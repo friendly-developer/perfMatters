b1bc7dadfbe090f0a9accc29f21c0d57868d1364
gvsvarun <gvsvarun@gmail.com>
2017-02-03 08:00:55 +0530

    changes to centralize gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var filesize = require('gulp-filesize');
 // var watch = require('gulp-watch');
 var htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
+var cleanCSS = require('gulp-clean-css');
+var livereload = require('gulp-livereload');
 
 gulp.task('clean', function () {
   return gulp.src('dist', {read: false})
@@ -17,28 +19,50 @@ gulp.task('clean', function () {
 });
 
 gulp.task('jsUglify',['clean'], function() {
-  return gulp.src('js/*.js')
+  return gulp.src(['js/*.js','views/js/*.js'],{base: './'})
     //.pipe(concat('vendor.js'))
     //.pipe(gulp.dest('dist'))
     .pipe(filesize())
     .pipe(uglify())
     //.pipe(rename('perfMatters.min.js'))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist'))
     .pipe(filesize())
     .on('error', gutil.log)
 });
 
 
 gulp.task('imagesMin', ['clean'], function() {
-  return gulp.src('img/*')
+  return gulp.src(['img/*',
+                'views/images/*' ],
+            {base: './'})
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('dist'))
+
 });
+
 gulp.task('minify',['clean'], function() {
-  return gulp.src('*.html')
+  return gulp.src(['*.html',
+                'views/*.html' ],
+            {base: './'})
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
-gulp.task('default', ['clean', 'jsUglify','minify','imagesMin'], function() {
 
+gulp.task('default', ['clean', 'jsUglify','minify','imagesMin','minify-css'], function() {
+
+});
+
+gulp.task('minify-css',['clean'], function() {
+  return gulp.src(['css/*.css','views/css/*.css'],
+            {base: './'})
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  livereload({ start: true })
+  livereload.listen();
+  gulp.watch('*.html', ['minify']);
+  gulp.watch('js/*.js', ['jsUglify']);
+  gulp.watch('css/*.css', ['minify-css']);
 });
--- views/js/main.js
+++ views/js/main.js
@@ -406,13 +406,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -463,8 +463,8 @@ var resizePizzas = function(size) {
         default:
           console.log("bug in sizeSwitcher");
       }
-  	var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
-    for (var i = 0; i < randomPizzas.length; i++) {
+  	var randomPizzas = document.getElementsByClassName("randomPizzaContainer");
+    for (var i = 0,len = randomPizzas.length; i < len; i++) {
       //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       // var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       randomPizzas[i].style.width = newwidth+'%';
@@ -483,8 +483,10 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+//
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
+
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
@@ -516,10 +518,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   var scrollTop = (document.body.scrollTop / 1250);
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(scrollTop + (i % 5));
+  for (var i = 0,len = items.length,phase; i < len; i++) {
+    phase = Math.sin(scrollTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -540,15 +542,16 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
+  var movingPizzas= document.getElementById("movingPizzas1");
+  for (var i = 0,elem; i < 200; i++) {
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
